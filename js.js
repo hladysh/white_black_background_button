99a88a0c84fd45232fb6fc84e49256b092c9275b
Ivan <hladysh.im@gmail.com>
2016-06-15 09:56:10 +0000

    creat js

---
+++ js.js
@@ -0,0 +1,6 @@
+        document.getElementsByTagName('button')[0].addEventListener('click', function(){
+                document.body.className='white';
+        });
+        document.getElementsByTagName('button')[1].addEventListener('click',function() {
+                document.body.className='black';
+        })
